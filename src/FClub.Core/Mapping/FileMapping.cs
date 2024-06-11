147ca01cd364cdb234d20b2614c1f737f6220cea
JOHNNY.X <2546088435@qq.com>
2024-06-11 15:20:59 +0800

    Update FileMapping.cs

--- src/FClub.Core/Mapping/FileMapping.cs
+++ src/FClub.Core/Mapping/FileMapping.cs
@@ -1,6 +1,6 @@
 using AutoMapper;
 using FClub.Core.Domain.File;
-using FClub.Messages.Commands;
+using FClub.Messages.Requests;
 
 namespace FClub.Core.Mapping;
 
@@ -8,6 +8,6 @@ public class FileMapping : Profile
 {
     public FileMapping()
     {
-        CreateMap<CombineMp4VideosTaskCommand, FClubFile>();
+        CreateMap<FClubFile, GetCombineMp4VideoTaskDto>();
     }
 }
