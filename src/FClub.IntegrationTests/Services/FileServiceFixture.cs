8ef0a7a1d8b8c598cc0b13de8c5ca4e04341346d
JOHNNY.X <2546088435@qq.com>
2024-06-13 16:06:22 +0800

    Update FileServiceFixture.cs

--- src/FClub.IntegrationTests/Services/FileServiceFixture.cs
+++ src/FClub.IntegrationTests/Services/FileServiceFixture.cs
@@ -7,16 +7,4 @@ namespace FClub.IntegrationTests.Services;
 
 public class FileServiceFixture : FileFixtureBase
 {
-    [Fact]
-    public async Task ShouldCombineAsync()
-    {
-        await Run<IMediator>(async mediator =>
-        {
-            var command = new CombineMp4VideosCommand
-            {
-                FilePath = "FClub/2dfab0fe-1b1f-4c90-9903-59b97cde2f4e.mp4",
-               
-            };
-        });
-    }
 }
